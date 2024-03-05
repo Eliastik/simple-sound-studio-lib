51d412f848532d192b5a8a0b538ac317668490b7
Eliastik <26941242+Eliastik@users.noreply.github.com>
2024-03-05 20:48:51 +0100

    Adjust smoothing factor

--- lib/model/Constants.ts
+++ lib/model/Constants.ts
@@ -62,7 +62,8 @@ const Constants = {
     VALID_SAMPLE_RATES: [0, 8000, 11025, 16000, 22050, 32000, 44100, 48000, 88200, 96000, 176400, 192000],
     // Interval used by the treatment percent counter. The event will be dispatched each ms defined here
     TREATMENT_TIME_COUNTING_THROTTLE_INTERVAL: 100,
-    TREATMENT_TIME_COUNTING_SMOOTHING_FACTOR: 0.7
+    // Smoothing factor for the time couting estimation (between 0 and 1)
+    TREATMENT_TIME_COUNTING_SMOOTHING_FACTOR: 0.9
 };
 
 export default Constants;
