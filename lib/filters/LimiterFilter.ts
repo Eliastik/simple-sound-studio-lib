13cd948f3ea0a137af0b39ab4089f75e04dbfa81
Eliastik <26941242+Eliastik@users.noreply.github.com>
2024-05-06 16:46:59 +0200

    Bug fix

--- lib/filters/LimiterFilter.ts
+++ lib/filters/LimiterFilter.ts
@@ -15,6 +15,7 @@ export default class LimiterFilter extends AbstractAudioFilterWorklet<void> {
 
     constructor() {
         super();
+        this.keepCurrentNodeIfPossible = true;
         this.enable();
         this.setDefaultEnabled(true);
     }
