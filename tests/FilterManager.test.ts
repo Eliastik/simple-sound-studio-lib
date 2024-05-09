658955a6ced115c2a518e0565838b87de2470726
Eliastik <26941242+Eliastik@users.noreply.github.com>
2024-05-09 16:36:53 +0200

    Add tests

--- tests/FilterManager.test.ts
+++ tests/FilterManager.test.ts
@@ -10,4 +10,46 @@ describe("FilterManager tests", () => {
             "limiter": true
         });
     });
+
+    test("Disable one filter", () => {
+        const filterManager = new FilterManager([new LimiterFilter()], null);
+
+        filterManager.toggleFilter("limiter");
+
+        expect(filterManager.getFiltersState()).toStrictEqual({
+            "limiter": false
+        });
+    });
+
+    test("Reset one filter state", () => {
+        const filterManager = new FilterManager([new LimiterFilter()], null);
+
+        filterManager.toggleFilter("limiter");
+
+        expect(filterManager.getFiltersState()).toStrictEqual({
+            "limiter": false
+        });
+
+        filterManager.resetAllFiltersState();
+
+        expect(filterManager.getFiltersState()).toStrictEqual({
+            "limiter": true
+        });
+    });
+
+    test("Change one filter settings", () => {
+        const filterManager = new FilterManager([new LimiterFilter()], null);
+
+        expect(filterManager.getFiltersSettings().get("limiter")).toMatchObject({
+            "attackTime": 0
+        });
+
+        filterManager.changeFilterSettings("limiter", {
+            "attackTime": 5
+        });
+
+        expect(filterManager.getFiltersSettings().get("limiter")).toMatchObject({
+            "attackTime": 5
+        });
+    });
 });
