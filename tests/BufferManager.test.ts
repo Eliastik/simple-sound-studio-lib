a4ed05e85204b9967b455610159f7d3685da05bd
Eliastik <26941242+Eliastik@users.noreply.github.com>
2024-05-13 21:01:40 +0200

    Add tests

---
+++ tests/BufferManager.test.ts
@@ -0,0 +1,64 @@
+import { describe, expect, test } from "@jest/globals";
+import "reflect-metadata";
+import BufferManager from "../lib/audioEditor/BufferManager";
+import EventEmitter from "../lib/utils/EventEmitter";
+import { EventType } from "../lib/model/EventTypeEnum";
+import FilterManager from "../lib/audioEditor/FilterManager";
+
+// Mock des dépendances
+const mockBufferFetcherService = {
+    fetchAllBuffers: jest.fn(),
+    reset: jest.fn()
+};
+const mockEventEmitter = new EventEmitter();
+const mockAudioBuffersToFetch = ["buffer1", "buffer2"];
+
+describe("BufferManager tests", () => {
+    test("Initialize BufferManager with audio buffers to fetch", () => {
+        const bufferManager = new BufferManager(new FilterManager([], null), mockBufferFetcherService, mockEventEmitter, mockAudioBuffersToFetch);
+
+        expect(bufferManager.downloadingInitialData).toBe(false);
+        expect((bufferManager as any).audioBuffersToFetch).toEqual(mockAudioBuffersToFetch);
+    });
+
+    test("Fetch buffers from network", async () => {
+        const bufferManager = new BufferManager(new FilterManager([], null), mockBufferFetcherService, mockEventEmitter, mockAudioBuffersToFetch);
+        
+        await bufferManager.resetBufferFetcher();
+
+        expect(mockBufferFetcherService.fetchAllBuffers).toHaveBeenCalledWith(mockAudioBuffersToFetch);
+    });
+
+    test("Emit loading buffers event", async () => {
+        const mockEventEmitterEmit = jest.spyOn(mockEventEmitter, "emit");
+        const bufferManager = new BufferManager(new FilterManager([], null), mockBufferFetcherService, mockEventEmitter, mockAudioBuffersToFetch);
+        
+        await bufferManager.resetBufferFetcher();
+
+        expect(mockEventEmitterEmit).toHaveBeenCalledWith(EventType.LOADING_BUFFERS);
+    });
+
+    test("Emit loaded buffers event after successful fetch", async () => {
+        const mockEventEmitterEmit = jest.spyOn(mockEventEmitter, "emit");
+        const bufferManager = new BufferManager(new FilterManager([], null), mockBufferFetcherService, mockEventEmitter, mockAudioBuffersToFetch);
+        
+        await bufferManager.resetBufferFetcher();
+
+        expect(mockEventEmitterEmit).toHaveBeenCalledWith(EventType.LOADED_BUFFERS);
+    });
+
+    test("Emit loading buffers error event on fetch failure", async () => {
+        const mockBufferFetcherServiceError = {
+            fetchAllBuffers: jest.fn(() => {
+                throw new Error();
+            }),
+            reset: jest.fn()
+        };
+        const mockEventEmitterEmit = jest.spyOn(mockEventEmitter, "emit");
+        const bufferManager = new BufferManager(new FilterManager([], null), mockBufferFetcherServiceError, mockEventEmitter, mockAudioBuffersToFetch);
+        
+        await bufferManager.resetBufferFetcher();
+
+        expect(mockEventEmitterEmit).toHaveBeenCalledWith(EventType.LOADING_BUFFERS_ERROR);
+    });
+});
