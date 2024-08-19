e4cf08d55683a5a6536ec618355ac1e6c14cd7a4
Eliastik <26941242+Eliastik@users.noreply.github.com>
2024-08-19 19:40:55 +0200

    Update eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,43 @@
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [{
+    ignores: ["**/*.js", "**/*.d.ts", "tests/**/*.ts"],
+}, ...compat.extends("eslint:recommended", "plugin:@typescript-eslint/recommended"), {
+    plugins: {
+        "@typescript-eslint": typescriptEslint,
+    },
+
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+        },
+
+        parser: tsParser,
+        ecmaVersion: "latest",
+        sourceType: "module",
+    },
+
+    rules: {
+        indent: ["warn", 4],
+        "linebreak-style": ["warn", "unix"],
+        quotes: ["error", "double"],
+        semi: "off",
+        "@typescript-eslint/no-unused-vars": "warn",
+        "@typescript-eslint/no-unused-expressions": "warn",
+        "prefer-const": "warn"
+    },
+}];
