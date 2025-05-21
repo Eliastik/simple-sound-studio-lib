6d97a019598202e3c71a9afa83d6493e087139e0
Eliastik <26941242+Eliastik@users.noreply.github.com>
2025-05-21 22:06:16 +0200

    Minor fix

--- lib/filters/ReturnAudioRenderer.ts
+++ lib/filters/ReturnAudioRenderer.ts
@@ -4,26 +4,24 @@ import Constants from "../model/Constants";
 export default class ReturnAudioRenderer extends AbstractAudioRenderer {
 
     renderAudio(context: BaseAudioContext, buffer: AudioBuffer): Promise<AudioBuffer> {
-        return new Promise(resolve => {
-            const numChannels = buffer.numberOfChannels;
-            const totalFrames = context.sampleRate * buffer.duration + context.sampleRate * 2;
-            const bufferReturned = context.createBuffer(numChannels, totalFrames, context.sampleRate);
+        const numChannels = buffer.numberOfChannels;
+        const totalFrames = context.sampleRate * buffer.duration + context.sampleRate * 2;
+        const bufferReturned = context.createBuffer(numChannels, totalFrames, context.sampleRate);
 
-            for (let channel = 0; channel < numChannels; channel++) {
-                const nowBuffering = bufferReturned.getChannelData(channel);
-                const sourceChannelData = buffer.getChannelData(channel);
+        for (let channel = 0; channel < numChannels; channel++) {
+            const nowBuffering = bufferReturned.getChannelData(channel);
+            const sourceChannelData = buffer.getChannelData(channel);
 
-                for (let i = 0; i < totalFrames; i++) {
-                    if (i < sourceChannelData.length) {
-                        nowBuffering[i] = sourceChannelData[sourceChannelData.length - 1 - i];
-                    } else {
-                        nowBuffering[i] = 0;
-                    }
+            for (let i = 0; i < totalFrames; i++) {
+                if (i < sourceChannelData.length) {
+                    nowBuffering[i] = sourceChannelData[sourceChannelData.length - 1 - i];
+                } else {
+                    nowBuffering[i] = 0;
                 }
             }
+        }
 
-            resolve(bufferReturned);
-        });
+        return Promise.resolve(bufferReturned);
     }
 
     get order(): number {
